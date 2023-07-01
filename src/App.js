c5f5f633d16e54fc681cfa91f3915f24d3795022
Pawan Kumar Dubey <spinifer_x@Pawans-MacBook-Pro.local>
2023-07-01 12:55:39 +0530

    layout changed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-import MouseParticles from 'react-mouse-particles'
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <MouseParticles g={0} color="#FD1C03" cull="col,image-wrapper" radius="12" num="3"/>
-      <header className="App-header">
-       <span className='App-logo'>🕹</span>
-        <p>
-          Welcome to <span style={{color: 'red'}}>Arcade</span> Theory
-        </p>
-      </header>
-    </div>
-  );
-}
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Header />
+    <AboutUs />
+    <SpecialMenu />
+    <Chef />
+    <Intro />
+    <Laurels />
+    <Gallery />
+    <FindUs />
+    <Footer />
+  </div>
+);
 
 export default App;
